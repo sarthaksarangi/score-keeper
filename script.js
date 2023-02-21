5d054bf3cbe2ffa34319b3db527054a114803832
sarthaksarangi <sarthakrohan5@gmail.com>
2023-02-21 18:02:11 +0530

    new

---
+++ script.js
@@ -0,0 +1,44 @@
+const p1 = document.querySelector('#p1Button')
+const p2 = document.querySelector('#p2Button')
+const p1Display = document.querySelector('#player1')
+const p2Display = document.querySelector('#player2')
+const resetButton = document.querySelector('#reset')
+const play  =document.querySelector('#playTo')
+
+let score1 = 0; 
+let score2 = 0;
+let winScore = 3;
+let gameOver = false
+p1.addEventListener('click',(e)=>{
+    if(!gameOver){
+        score1++;
+        if(score1===winScore){
+            gameOver = true       
+         }
+        p1Display.innerText = score1;
+    }   
+})
+p2.addEventListener('click',(e)=>{
+    if(!gameOver){
+        score2++;
+        if(score2===winScore){
+            gameOver = true       
+         }
+        p2Display.innerText = score2;
+    }   
+})
+resetButton.addEventListener('click', resetGame)
+  
+
+play.addEventListener('change',function(){
+    winScore = parseInt(this.value);
+    resetGame()
+})
+
+function resetGame(){
+    gameOver = false
+    score1 = 0;
+    score2 = 0;
+    document.querySelector('#player1').innerText = score1;
+    document.querySelector('#player2').innerText = score2;
+}
